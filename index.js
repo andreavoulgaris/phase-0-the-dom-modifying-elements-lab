08d8424122f5492350ea504325831d8c87fbb1cb
Andrea Voulgaris <abv@umich.edu>
2022-11-22 12:05:46 -0500

    commit tree d1899ed532e8007e91162b84cd9f881d7da10e07

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+
+const main = document.querySelector('#main').remove();
+console.log(document.querySelector('#main'));
+const newHeader = document.createElement("h1");
+console.log(newHeader);
+newHeader.id = "victory";
+newHeader.innerHTML = "Andrea is the champion";
+
